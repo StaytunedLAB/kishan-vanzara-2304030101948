1a57655d3879a5380846e3ede5677ff38d3a6530
sahil0108yadav <2304030102017@silveroakuni.ac.in>
2025-12-17 22:58:31 -0800

    commit

---
+++ docs/createissue.js
@@ -0,0 +1,21 @@
+import { Octokit } from "@octokit/rest";
+
+// Create Octokit instance with your GitHub token
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN, // or "ghp_xxx"
+});
+
+async function createIssue() {
+  const response = await octokit.issues.create({
+    owner: "OWNER_NAME",
+    repo: "REPO_NAME",
+    title: "Bug: Something is broken",
+    body: "### Description\nThis issue was created using Octokit.",
+    labels: ["bug", "automation"],
+    assignees: ["github-username"],
+  });
+
+  console.log("Issue created:", response.data.html_url);
+}
+
+createIssue().catch(console.error);
