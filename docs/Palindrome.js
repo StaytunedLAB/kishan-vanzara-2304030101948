fab413ed57cc8b3cf69b20fe6022c9aa64461e41
sahil0108yadav <2304030102017@silveroakuni.ac.in>
2025-12-18 23:16:25 -0800

    commit

--- docs/Palindrome.js
+++
@@ -1,8 +0,0 @@
-let str = "jayesh";
-
-let rev = str.split("").reverse().join("");
-
-if (str === rev)
-    console.log("Palindrome String");
-else
-    console.log("Not a Palindrome String");
--- docs/leap_year.js
+++
@@ -1,5 +0,0 @@
-let year =2024
-if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-    console.log(year + " is a Leap Year");
-else
-    console.log(year + " is Not a Leap Year");
